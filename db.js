c72c0bf6d55bc67219a497b7f9117402db3fcd26
lilyyang <1592218619@qq.com>
2022-09-27 21:15:29 +0800

    测试读写文件

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const home = process.env.HOME || homedir;
 const p = require("path");
 const fs = require("fs");
 const dbPath = p.join(home, ".todo");
+
 const db = {
   read(path = dbPath) {
     return new Promise((resolve, reject) => {
